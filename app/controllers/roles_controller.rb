8c02f99873da55abc06d45c0b644449f2a5e00c8
Gladki <alexanderglad@gmail.com>
2013-06-23 14:21:53 +0200

    change roles

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,6 +1,6 @@
-class RolesController < ApplicationController
+ class RolesController < ApplicationController
 # load_and_authorize_resource
-  before_filter :authenticate_user!
+  # before_filter :authenticate_user!
 
   # GET /roles
   # GET /roles.json
